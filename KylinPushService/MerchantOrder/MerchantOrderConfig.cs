dfc2ec77b9ef78a714410d94b86416a4d2926a52
袁坷 <562573053@qq.com>
2016-05-11 10:02:17 +0800

    商家订单服务开发

---
+++ KylinPushService/MerchantOrder/MerchantOrderConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KylinPushService.MerchantOrder
+{
+    /// <summary>
+    /// 商家订单配置
+    /// </summary>
+    public class MerchantOrderConfig
+    {
+        /// <summary>
+        /// 上门预约Redis存储的数据库索引
+        /// </summary>
+        public static int DbIndex
+        {
+            get
+            {
+                int index = 0;
+
+                int.TryParse(ConfigurationManager.AppSettings["RedisAppointDbIndex"], out index);
+
+                return index;
+            }
+        }
+
+        /// <summary>
+        /// 商家订单在Redis中存储的Key
+        /// </summary>
+        public static class RedisKey
+        {
+            /// <summary>
+            /// 商家订单用户付款
+            /// </summary>
+            public static string PayMerchantOrder { get { return ConfigurationManager.AppSettings["PayMerchantOrder"]; } }
+            /// <summary>
+            /// 商家订单商家发货
+            /// </summary>
+            public static string SendMerchantOrder { get { return ConfigurationManager.AppSettings["SendMerchantOrder"]; } }
+            /// <summary>
+            /// 商家订单用户确认收货
+            /// </summary>
+            public static string ConfirmMerchantOrder { get { return ConfigurationManager.AppSettings["ConfirmMerchantOrder"]; } }
+
+        }
+    }
+}
---
+++ KylinPushService/MerchantOrder/SendOrder/SendOrderContent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KylinPushService.MerchantOrder.SendOrder
+{
+    /// <summary>
+    /// 商家订单商家发货
+    /// </summary>
+    public class SendOrderContent
+    {
+
+        /// <summary>
+        /// 订单ID
+        /// </summary>
+        public long OrderID { get; set; }
+        /// <summary>
+        /// 订单编号
+        /// </summary>
+        public string OrderCode { get; set; }
+        /// <summary>
+        /// 商家ID
+        /// </summary>
+        public long MerchantID { get; set; }
+
+        ///<summary>
+        ///实际订单总金额
+        ///</summary>
+        public decimal ActualOrderAmount { get; set; }
+        ///<summary>
+        ///买家用户ID
+        ///</summary>
+        public long UserID { get; set; }
+
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string UserName { get; set; }
+
+        ///<summary>
+        ///付款时间
+        ///</summary>
+
+        public DateTime SendTime { get; set; }
+    }
+}
