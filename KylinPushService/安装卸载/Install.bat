5c000c65045776cd6ef5d1ec38b42e62663f4687
yk <yk@DESKTOP-97CR00P>
2016-07-13 18:20:14 +0800

    BUG修复

--- KylinPushService/安装卸载/Install.bat
+++ KylinPushService/安装卸载/Install.bat
@@ -1,6 +1,6 @@
 @echo.服务启动......  
 @echo off  
-@sc create KylinPushService binPath= "D:\20151120Git\Td.Kylin.PushService\KylinPushService\bin\Debug\KylinPushService.exe"  
+@sc create KylinPushService binPath= "F:\20151120Git\Td.Kylin.PushService\KylinPushService\bin\Debug\KylinPushService.exe"  
 @net start KylinPushService  
 @sc description KylinPushService "Kylin消息推送服务"
 @sc config KylinPushService start= AUTO  
